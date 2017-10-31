fe0b3d8db83ac9f31c01191631619b6667c91942
aljoh <reachingalfred@gmail.com>
2017-10-31 19:37:01 +0100

    added instance double person and owner for account on initialize

--- lib/account.rb
+++ lib/account.rb
@@ -1,12 +1,23 @@
 class Account
-  attr_accessor :pin_code, :balance, :account_status
-  def initialize
+  attr_accessor :pin_code, :balance, :account_status, :owner
+  def initialize(attrs = {})
     @pin_code = rand(1000..9999)
     @balance = 0
     @account_status = :active
+    set_owner(attrs[:owner])
   end
 
   def deactivate
     @account_status = :deactivated
+  end
+
+  private
+
+  def set_owner(obj)
+    obj == nil ? missing_owner : @owner = obj
+  end
+
+  def missing_owner
+    raise "An Account owner is required"
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,6 +1,14 @@
 require './lib/account.rb'
 
 describe Account do
+  let(:person) { instance_double('Person', name: 'Thomas') }
+  subject { described_class.new({owner: person}) }
+  it 'account has an owner' do
+    expect(subject.owner).to eq person
+  end
+  it 'it raises an error if no owner is set' do
+    expect { described_class.new}.to raise_error 'An Account owner is required'
+  end
   it 'it has a 4 digit pin-code on initialize' do
     expect(subject.pin_code.to_s.length).to eq 4
   end
