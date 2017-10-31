2e7b1721c69e2b88a09b7ba9849df2c2fd38ef7a
aljoh <reachingalfred@gmail.com>
2017-10-31 20:12:55 +0100

    added error message if no name is set on initialize

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,16 @@
 class Person
   attr_accessor :name
-  def initialize(name)
-    @name = name
+  def initialize(name = nil)
+    set_name(name)
+  end
+
+  private
+
+  def set_name(name)
+    if name == nil
+      raise 'A name is required'
+    else
+      @name = name
+    end
   end
 end
