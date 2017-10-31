10469255bce27142e4e92c822e2b946f6a9db844
aljoh <reachingalfred@gmail.com>
2017-10-31 14:16:07 +0100

    added balance to account on initialize

--- lib/account.rb
+++ lib/account.rb
@@ -1,6 +1,7 @@
 class Account
-  attr_accessor :pin_code
+  attr_accessor :pin_code, :balance
   def initialize
-    @pin_code = rand(1000..9999).to_s
+    @pin_code = rand(1000..9999)
+    @balance = 0
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,6 +2,9 @@ require './lib/account.rb'
 
 describe Account do
   it 'it has a 4 digit pin-code on initialize' do
-    expect(subject.pin_code.length).to eq 4
+    expect(subject.pin_code.to_s.length).to eq 4
+  end
+  it 'it has a balance of 0$ on initialize' do
+    expect(subject.balance).to eq 0
   end
 end
