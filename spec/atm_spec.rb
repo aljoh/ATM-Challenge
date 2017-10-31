a571e0f8ab3220e55363ff013388a27b43447d1f
aljoh <reachingalfred@gmail.com>
2017-10-31 10:57:53 +0100

    added check for account status

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,7 +1,7 @@
 require './lib/atm.rb'
 
 describe Atm do
-  let(:account) { instance_double('Account', pin_code: '1234', exp_date: '12/17') }
+  let(:account) { instance_double('Account', pin_code: '1234', exp_date: '12/17', account_status: :active) }
 
   before do
     allow(account).to receive(:balance).and_return(100)
@@ -36,5 +36,10 @@ describe Atm do
     allow(account).to receive(:exp_date).and_return('12/15')
     expected_output = { status: false, message: 'card expired', date: Date.today }
     expect(subject.withdraw(6, '1234', account)).to eq expected_output
+  end
+  it 'reject withdraw if account is disabled' do
+    allow(account).to receive(:account_status).and_return(:disabled)
+    expected_output = { status: false, message: 'account disabled', date: Date.today }
+    expect(subject.withdraw(5, '1234', account)).to eq expected_output
   end
 end
