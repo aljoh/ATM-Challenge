ea418386273cf5c6a872b0d8c8d9b38e6b9cad9c
aljoh <reachingalfred@gmail.com>
2017-10-31 11:24:14 +0100

    added bills for withdraw

--- lib/atm.rb
+++ lib/atm.rb
@@ -36,7 +36,7 @@ class Atm
   def perform_transaction(amount, account)
     @funds -= amount
     account.balance -= amount
-    { status: true, message: 'success', date: Date.today, amount: amount }
+    { status: true, message: 'success', date: Date.today, amount: amount, bills: add_bills(amount) }
   end
 
   def incorrect_pin?(pin_code, actual_pin)
@@ -49,5 +49,17 @@ class Atm
 
   def account_disabled?(account_status)
     account_status != :active
+  end
+
+  def add_bills(amount)
+    denominations = [20, 10, 5]
+    bills = []
+    denominations.each do |bill|
+      while amount - bill >= 0
+        amount -= bill
+        bills << bill
+      end
+    end
+    bills
   end
 end
