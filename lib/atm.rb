e8123e6a733cd1b1c15ac8baa00845de63541392
aljoh <reachingalfred@gmail.com>
2017-10-31 09:48:05 +0100

    added reject withdraw if pin is incorrect

--- lib/atm.rb
+++ lib/atm.rb
@@ -6,10 +6,14 @@ class Atm
     @funds = 1000
   end
 
-  def withdraw(amount, account)
+  def withdraw(amount, pin_code, account)
     case
     when insufficient_funds_in_account?(amount, account)
       { status: false, message: 'insufficient funds', date: Date.today }
+    when insufficient_funds_in_atm?(amount)
+      { status: false, message: 'insufficient funds in ATM', date: Date.today }
+    when incorrect_pin?(pin_code, account.pin_code)
+      { status: false, message: 'wrong pin', date: Date.today }
     else
       perform_transaction(amount, account)
     end
@@ -21,9 +25,17 @@ class Atm
     amount > account.balance
   end
 
+  def insufficient_funds_in_atm?(amount)
+    @funds < amount
+  end
+
   def perform_transaction(amount, account)
     @funds -= amount
     account.balance -= amount
     { status: true, message: 'success', date: Date.today, amount: amount }
+  end
+
+  def incorrect_pin?(pin_code, actual_pin)
+    pin_code != actual_pin
   end
 end
