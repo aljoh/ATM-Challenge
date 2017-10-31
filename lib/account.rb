1397c024ebadc46ab0c206ddd44e9115f2748246
aljoh <reachingalfred@gmail.com>
2017-10-31 14:30:51 +0100

    added deactivate account method

--- lib/account.rb
+++ lib/account.rb
@@ -1,7 +1,12 @@
 class Account
-  attr_accessor :pin_code, :balance
+  attr_accessor :pin_code, :balance, :account_status
   def initialize
     @pin_code = rand(1000..9999)
     @balance = 0
+    @account_status = :active
+  end
+
+  def deactivate
+    @account_status = :deactivated
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -7,4 +7,10 @@ describe Account do
   it 'it has a balance of 0$ on initialize' do
     expect(subject.balance).to eq 0
   end
+  it 'it has account status :active on initialize' do
+    expect(subject.account_status).to eq :active
+  end
+  it 'deactivates account with deactivate method' do
+    expect(subject.deactivate).to eq :deactivated
+  end
 end
